abd2c01d4daecf6c06d9634fb90ce6717bda386a
renium77 <54940001+renium77@users.noreply.github.com>
2021-11-16 01:50:44 +0530

    Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,23 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+  extendTheme,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Navbar from './components/Navbar';
 
 function App() {
+
+  const theme = extendTheme({
+  fonts: {
+    heading: "DM Sans",
+    body: "DM Sans",
+  },
+})
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Navbar/>
     </ChakraProvider>
   );
 }
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Box, Heading, Stack, Text, Flex, Link, Spacer } from '@chakra-ui/layout'
+import { Button, Menu, IconButton, extendTheme } from '@chakra-ui/react'
+import { AiOutlineMenu } from "react-icons/ai";
+import { useDisclosure } from '@chakra-ui/hooks';
+import { Drawer,DrawerContent, DrawerBody, DrawerOverlay, DrawerHeader } from '@chakra-ui/modal';
+
+function Navbar() {
+
+  const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat",
+    body: "Montserrat",
+  },
+})
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return (
+        <div>
+        <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        padding={6}
+        bg="white"
+        color="black"
+        opacity="30"
+        >
+
+      <Flex align="center" >
+        <Link>
+            <Heading as="h1" size="lg" textDecoration='none'>
+                ninad dere
+            </Heading>
+        </Link>
+      </Flex>
+      <Spacer/>
+
+      
+    <Menu display={{ base: "inline-flex", md: "none" }}>
+        <Box>
+        <Button
+            as={IconButton}
+            aria-label="Options"
+            border='black'
+            icon={<AiOutlineMenu />}
+            bgColor="#none"
+            variant="outline"
+            display={{ base: "inline-flex", md: "none" }}
+            onClick={onOpen}
+        />
+        <Drawer placement="bottom" onClose={onClose} isOpen={isOpen} size="xs">
+          <DrawerOverlay/>
+                                    <DrawerContent>
+                                      <DrawerBody>
+                                        <DrawerHeader borderBottomWidth="1px"><Link to="/international" color="black" fontSize="lg">International Tours</Link></DrawerHeader>
+                                        <DrawerHeader borderBottomWidth="1px"><Link to="/domestic" color="black" fontSize="lg">Domestic Tours</Link></DrawerHeader>
+                                        <DrawerHeader borderBottomWidth="1px"><Link to="/customise" color="black" fontSize="lg">Customise</Link></DrawerHeader>
+                                        <DrawerHeader borderBottomWidth="1px"><Link to='/testimonial' color="black" fontSize="lg">Testimonials</Link></DrawerHeader>
+                                        <DrawerHeader borderBottomWidth="1px"><Link to='/blog' color="black" fontSize="lg">More</ Link></DrawerHeader>
+                                        <DrawerHeader borderBottomWidth="1px"><Link to='/contactus' color="black" fontSize="lg">Contact Us</ Link></DrawerHeader>
+                                      </DrawerBody>
+                                    </DrawerContent>
+        </Drawer>
+        </Box>
+        </Menu>
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        display={{ base: "none", md: "inline-flex" }}
+        width={{ base: "full", md: "auto" }}
+        justifyContent="end"
+        flexGrow={1}
+        mt={{ base: 4, md: 0 }}
+        spacing={5}
+        border='black'
+      >
+        <Button border='2px' borderRadius='20' fontSize="lg" bg='white' color="black" _hover={{bg:'black', color:'white'}}>projects</Button>
+        <Button border='2px' borderRadius='20' fontSize="lg" bg='white' color="black" _hover={{bg:'black', color:'white'}}>about</Button>
+        <Button border='2px' borderRadius='20' fontSize="lg" bg='white' color="black" _hover={{bg:'black', color:'white'}}>posts</Button>
+        <Button border='2px' borderRadius='20' fontSize="lg" bg='white' color="black" _hover={{bg:'black', color:'white'}}>work</Button>
+        <Button border='2px' borderRadius='20' fontSize="lg" bg='white' color="black" _hover={{bg:'black', color:'white'}}>links</Button>
+      </Stack>
+      
+
+      
+    </Flex>
+        </div>
+    )
+}
+
+export default Navbar
+
